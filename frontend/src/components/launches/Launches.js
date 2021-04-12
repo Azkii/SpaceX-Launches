5b5904d70319ae79192cc7facd61e4190cd5552d
Dawid <Dawid_pacanowski@wp.pl>
2021-04-12 21:56:06 +0200

    main query completed launch item added

--- frontend/src/components/launches/Launches.js
+++ frontend/src/components/launches/Launches.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { gql, useQuery } from '@apollo/client';
+import LaunchItem from './LaunchItem';
 
 
 const LAUNCHES_QUERY = gql`
@@ -17,10 +18,12 @@ function Launches() {
     const { loading, error, data } = useQuery(LAUNCHES_QUERY);
     if (error) return `Error! ${error}`;
     if (loading) return 'Loading...';
-    if (data) console.log(data);
     return (
         <div>
             <h1 className="display-4 my-3">Launches</h1>
+            {data.launches?.map((launch,index) => {
+                return <LaunchItem key={`${launch.flight_number}-${index}`} launch={launch} />
+            })}
         </div>
     )
 }
